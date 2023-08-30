7d06365cb634601fb701327e58e9bb224f506fd2
Marvin Müller <marvin.mueller99@mail.de>
2023-08-30 03:08:48 +0200

    Added Unit Tests for MemberAccessor

---
+++ RuntimeDiagnosis.Core.UnitTests/ObjectDiagnosis/MemberDiagnosis/Kit/MemberAccessorTests.cs
@@ -0,0 +1,78 @@
+using System.Linq.Expressions;
+using JetBrains.Annotations;
+using RuntimeDiagnosis.Core.ObjectDiagnosis.MemberDiagnosis.Kit;
+
+namespace RuntimeDiagnosis.Core.UnitTests.ObjectDiagnosis.MemberDiagnosis.Kit;
+
+[TestFixture]
+public class MemberAccessorTests
+{
+    private const string SkipSetup = "SkipSetup";
+    private IMemberAccessorInternal<string> _accessor = null!;
+
+    private string TestProperty { get; [UsedImplicitly] set; } = "Test";
+    
+    
+    [SetUp]
+    public void SetUp()
+    {
+        if (CheckForSkipSetup())
+            return;
+
+        _accessor = new MemberAccessor<string>();
+        _accessor.Initialize(() => TestProperty);
+    }
+
+    [Test]
+    public void ValueSetter_Getter_ReturnsNotNull()
+    {
+        // Act
+        var result = _accessor.Value;
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+    }
+
+    [Test]
+    public void ValueSetter_Getter_ReturnsCorrectValue()
+    {
+        // Act
+        var result = _accessor.Value;
+
+        // Assert
+        Assert.That(result, Is.EqualTo(TestProperty));
+    }
+
+    [Test]
+    public void ValueSetter_SetsValue_CorrectlyRetrieved()
+    {
+        // Arrange
+        const string newValue = "New Value";
+
+        // Act
+        _accessor.Value = newValue;
+
+        // Assert
+        Assert.That(TestProperty, Is.Not.Null);
+        Assert.That(TestProperty, Is.EqualTo(newValue));
+    }
+
+    [Test]
+    [Category(SkipSetup)]
+    public void Initialize_PassNonMemberExpression_ThrowsArgumentException()
+    {
+        // Arrange
+        IMemberAccessorInternal<double> accessor = new MemberAccessor<double>();
+        Expression<Func<double>> expression = () => 123.45;
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => accessor.Initialize(expression),
+            $"{nameof(expression)} must return a field or property");
+    }
+
+    private static bool CheckForSkipSetup()
+    {
+        var categories = TestContext.CurrentContext.Test.Properties["Category"];
+        return categories.Contains(SkipSetup);
+    }
+}
--- RuntimeDiagnosis.Core/ObjectDiagnosis/MemberDiagnosis/Kit/IMemberAccessorInternal.cs
+++ RuntimeDiagnosis.Core/ObjectDiagnosis/MemberDiagnosis/Kit/IMemberAccessorInternal.cs
@@ -4,5 +4,5 @@ namespace RuntimeDiagnosis.Core.ObjectDiagnosis.MemberDiagnosis.Kit;
 
 public interface IMemberAccessorInternal<T> : IMemberAccessor<T>
 {
-    internal void Initialize(Expression<Func<T?>> expression);
+    void Initialize(Expression<Func<T?>> expression);
 }
